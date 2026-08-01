b0b196a9d5a46ee472f899533b2ba97f0360d921
labcows <159016743+labcows@users.noreply.github.com>
2026-08-01 17:26:17 -0600

    ThreadPoolTests

---
+++ BlueFootedBoobyTests/ThreadPoolTests.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include <atomic>
+#include "ThreadPool.h"
+
+TEST(ThreadPoolTest, EveryThreadExecutesOnlyOneTime)
+{
+    ThreadPool pool(16);
+    std::atomic<int> counter{ 0 };
+    const int jobCount = 1000;
+
+    for (int i = 0; i < jobCount; i++)
+        pool.Submit([&counter] { counter++; });
+
+    pool.WaitAll();
+    EXPECT_EQ(counter, jobCount);
+}   
+
+TEST(ThreadPoolTest, ThreadPoolWaitsAllWorkers)
+{
+    ThreadPool pool(4);
+    std::atomic<int> counter{ 0 };
+
+    for (int i = 0; i < 20; i++)
+        pool.Submit([&counter] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        counter++;
+            });
+
+    pool.WaitAll();
+    EXPECT_EQ(counter, 20);
+}
+
+TEST(ThreadPoolTest, )
+{
+    ThreadPool pool(8);
+    std::atomic<int> counter{ 0 };
+
+    for (int i = 0; i < 100; i++) pool.Submit([&counter] { counter++; });
+    pool.WaitAll();
+
+    for (int i = 0; i < 100; i++) pool.Submit([&counter] { counter++; });
+    pool.WaitAll();
+    EXPECT_EQ(counter, 200);
+}
